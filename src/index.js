402049ec4c63861271bf35ce85f5d34f6b2830a8
KarthikGR <54638049+AlanWalker987@users.noreply.github.com>
2024-09-05 17:28:35 +0530

    conditional render and css changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -13,26 +58,78 @@ function App() {
 }
 
 function Header() {
-  const headerStyle = {
-    color: "red",
-    fontSize: "28px",
-    textTransform: "uppercase",
-  };
+  // const headerStyle = {
+  //   color: "red",
+  //   fontSize: "28px",
+  //   textTransform: "uppercase",
+  // };
+  const headerStyle = {};
   return (
-    <div>
+    <header className="header">
       <h1 style={headerStyle}>Fast React Pizza Co.</h1>
-    </div>
+    </header>
   );
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const pizzaCount = pizzas.length > 0;
+
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
+
+      {pizzaCount > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza
+                name={pizza.name}
+                ingredients={pizza.ingredients}
+                price={pizza.price}
+                photoName={pizza.photoName}
+                soldOut={pizza.soldOut}
+                key={pizza.name}
+              />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We are working on it. Please come back later.</p>
+      )}
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredient="Tomato, mozarella, spinach and ricotta cheese"
+        price={10}
+        photoName="pizzas/spinaci.jpg"
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredient="Tomato, mozarella, spinach and ricotta cheese"
+        price={15}
+        photoName="pizzas/funghi.jpg"
+      /> */}
+    </main>
+  );
+}
+
+function Pizza({ photoName, name, ingredients, price, soldOut }) {
+  return (
+    <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
+      <img src={photoName} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{soldOut ? "SOLD OUT" : price}</span>
+      </div>
+    </li>
   );
 }
 
@@ -46,19 +143,14 @@ function Footer() {
     : "Please come tomorrow!";
 
   return (
-    <footer>
-      {new Date().toLocaleTimeString()} {isOpenMessage}
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closedHour}:00. Come visit us or order online</p>
+          <button className="btn">Order Now</button>
+        </div>
+      )}
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="pizza" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach and ricotta cheese</p>
-    </div>
   );
 }
 
